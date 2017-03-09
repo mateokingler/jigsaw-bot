c727eeba1f561813af07383f861bc4b9b2dbf973
Mateo Aguirre <info@mateoaguirre.com>
2017-03-09 17:14:58 -0500

    Added screen resolution validation

--- jigsaw-bot.lua
+++ jigsaw-bot.lua
@@ -2,13 +2,6 @@
 -- TODO: Change function i values to reflect actual time
 --
 
--- Settings prompt
-local promptLabel = {type=CONTROLLER_TYPE.LABEL, text="jigsaw-bot settings:"}
-local promptInput = {type= CONTROLLER_TYPE.PICKER, title="Length", key="Length", value="5 minutes", options={"5 minutes", "20 minutes", "1 hour", "Stop manually"}}
-local promptCloseInfo = {type=CONTROLLER_TYPE.LABEL, text="Hold 'volume down' button to end script"}
-local controllers = {promptLabel, promptInput, promptCloseInfo};
-dialog(controllers, false);
-
 -- Iteration functions
 function fiveMinutes()
   local i = 1;
@@ -33,7 +26,6 @@ function fiveMinutes()
     end
   end
 end
-
 
 function twentyMinutes()
   local i = 1;
@@ -83,7 +75,6 @@ function oneHour()
   end
 end
 
-
 -- FIX MANUAL STOP (Detect user 3 click home button input)
 function manualStop()
   local i = 1;
@@ -109,12 +100,38 @@ function manualStop()
 end
 
 -- Main program loop
-if (promptInput.value == "5 minutes")
-  then fiveMinutes();
-elseif (promptInput.value == "20 minutes")
-  then twentyMinutes();
-elseif (promptInput.value == "1 hour")
-  then oneHour();
-elseif (promptInput.value == "Stop manually")
-  then manualStop();
+function iterationSelect()
+  if (promptInput.value == "5 minutes")
+    then fiveMinutes();
+  elseif (promptInput.value == "20 minutes")
+    then twentyMinutes();
+  elseif (promptInput.value == "1 hour")
+    then oneHour();
+  elseif (promptInput.value == "Stop manually")
+    then manualStop();
+  end
+end
+
+-- Settings prompt
+function runSettings()
+  promptLabel = {type=CONTROLLER_TYPE.LABEL, text="jigsaw-bot settings:"}
+  promptInput = {type= CONTROLLER_TYPE.PICKER, title="Length", key="Length", value="5 minutes", options={"5 minutes", "20 minutes", "1 hour", "Stop manually"}}
+  promptCloseInfo = {type=CONTROLLER_TYPE.LABEL, text="Hold 'volume down' button to end script"}
+  controllers = {promptLabel, promptInput, promptCloseInfo};
+  dialog(controllers, false);
+  iterationSelect();
+end
+
+-- Detect screen resolution, if true pass to runSettings()
+local screenWidth = getScreenResolution();
+if screenWidth == 640 -- iPhone 5/5S/5C/SE
+  then runSettings();
+  elseif screenWidth == 750 -- iPhone 6
+	then alert("Sorry, jigsaw-bot does not currently support iPhone 6s");
+  elseif screenWidth == 750 -- iPhone 7
+	then alert("Sorry, jigsaw-bot does not currently support iPhone 7s");
+  elseif screenWidth == 1242 -- iPhone 7+
+	then alert("Sorry, jigsaw-bot does not currently support the iPhone 7+");
+  else
+	alert("Sorry, jigsaw-bot does not currently support this device");
 end
